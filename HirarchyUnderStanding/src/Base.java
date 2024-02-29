39dadecdcf5bf6789b7a5619c4468afed4826685
Annas5585 <105589036+Anu5585@users.noreply.github.com>
2024-02-29 20:02:51 +0500

    Add files via upload

---
+++ HirarchyUnderStanding/src/Base.java
@@ -0,0 +1,11 @@
+public class Base {
+
+    void method1(){
+        System.out.println("1st Base.method1()");
+        method2();
+    }
+    void method2(){
+        System.out.println("Base.method2()");
+    }
+
+}
---
+++ HirarchyUnderStanding/src/Derived.java
@@ -0,0 +1,12 @@
+public class Derived extends Base {
+    void method1() {
+        System.out.println("OverRide Derived.method1()");
+        super.method1();
+    }
+
+    void method2() {
+        System.out.println("Dervied.method2()");
+
+    }
+}
+
