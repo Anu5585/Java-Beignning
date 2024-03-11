ae849a32eaf0178694a829122bbddb87568f477b
Annas5585 <105589036+Anu5585@users.noreply.github.com>
2024-03-11 23:12:38 +0500

    Add files via upload

---
+++ ObersverDeginPattern UI Handlig/src/Archiver.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class Archiver implements Observer 
+{
+  public Archiver() 
+  {
+  }
+	
+  public void update(String operation, String record)
+  {
+    System.out.println("The archiver says a " + operation +
+      " operation was performed on " + record);
+  }
+}
---
+++ ObersverDeginPattern UI Handlig/src/Client.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+
+public class Client implements Observer 
+{
+  public Client() 
+  {
+  }
+	
+  public void update(String operation, String record)
+  {
+    System.out.println("The client says a " + operation +
+      " operation was performed on " + record);
+  }
+}
