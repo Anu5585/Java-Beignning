f54ade4014ea03a663bc285a269b1dbbdb3142fe
Annas5585 <105589036+Anu5585@users.noreply.github.com>
2024-05-27 12:03:10 +0500

    Add files via upload

---
+++ javaFxMeaven/src/main/java/com/example/javafxmeaven/MyViewController.java
@@ -0,0 +1,23 @@
+package com.example.javafxmeaven;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MyViewController {
+
+    @FXML
+    private Button ButtonHello;
+
+    @FXML
+    private Label Message;
+
+    @FXML
+    void btnClicked(ActionEvent event) {
+
+    }
+
+}
+
---
+++ javaFxMeaven/test/com/example/javafxmeaven/HelloApplicationTest.java
@@ -0,0 +1,15 @@
+package com.example.javafxmeaven;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApplicationTest {
+
+    @Test
+    public void test(){
+        Assertions.assertNotNull("Islamabad","Annas");
+    }
+
+}
