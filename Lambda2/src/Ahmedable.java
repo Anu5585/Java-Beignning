31f73c40546755429945d35a9cad70da1e3a951e
Annas5585 <105589036+Anu5585@users.noreply.github.com>
2024-05-27 12:46:36 +0500

    Add files via upload

---
+++ Lambda2/src/Ahmedable.java
@@ -0,0 +1,10 @@
+@FunctionalInterface
+public interface Ahmedable {
+
+    //shared constants public static final
+    int i=0;
+    //methods abstract
+    int foo(int i);
+    default void boo(){}
+
+}
---
+++ Lambda2/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+    //step 1 Write te lambda expression
+        //Step 2 assign it to atype
+        // call the method
+       Ahmedable mm =  x-> 10;
+
+        System.out.println(mm.foo(10));
+
+
+
+    }
+}
